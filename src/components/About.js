ead12c2a90be295b6e11052c0ece956406f91dce
SmartAzanClock <smartazanclock@gmail.com>
2023-01-04 23:08:32 -0500

    about simplified

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
     return (
         <div>
 
-            <h5>As-Salamu Alaykum</h5>
+            <h5>As Salamu Alaykum</h5>
             <p></p>
             SmartAzanClock.com is an online adhan clock which can also be used to turn any Android or Raspberry Pi device into an actual adhan clock.
             <p></p>
@@ -14,71 +14,66 @@ export default function About() {
             <div className='d-flex flex-column gap-2'>
                 <div className='d-flex flex-row'>
                     <div className='mx-1'>{FontAwesome.Check}</div>
-                    <div>It's free, safe and open source.</div>
+                    <div>It's free, safe and open source</div>
                 </div>
 
                 <div className='d-flex flex-row'>
                     <div className='mx-1'>{FontAwesome.Check}</div>
-                    <div>There's no registration, no subscriptions, no app store downloads or browser cookies.</div>
+                    <div>There's no registration</div>
                 </div>
 
                 <div className='d-flex flex-row'>
                     <div className='mx-1'>{FontAwesome.Check}</div>
-                    <div>It works even when offline.</div>
+                    <div>No subscriptions</div>
                 </div>
 
                 <div className='d-flex flex-row'>
                     <div className='mx-1'>{FontAwesome.Check}</div>
-                    <div>It doesn't require any specific Android device or brand.</div>
+                    <div>No app store downloads</div>
+                </div>
+
+                <div className='d-flex flex-row'>
+                    <div className='mx-1'>{FontAwesome.Check}</div>
+                    <div>No browser cookies</div>
+                </div>
+
+                <div className='d-flex flex-row'>
+                    <div className='mx-1'>{FontAwesome.Check}</div>
+                    <div>It works even when offline</div>
+                </div>
+
+                <div className='d-flex flex-row'>
+                    <div className='mx-1'>{FontAwesome.Check}</div>
+                    <div>It doesn't require any specific Android device or brand</div>
                 </div>
             </div>
 
             <p></p>
 
-            <div className='mb-3 subMenu'>
-                <ul className="nav nav-pills" id="pills-tab" role="tablist">
-
-                    <li className="nav-item" role="presentation">
-                        <button className="nav-link btn-sm rounded-0 rounded-top active" data-bs-toggle="pill" data-bs-target="#anrdoid-instructions" type="button">
-                            {FontAwesome.Android} Android
-                        </button>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                        <button className="nav-link btn-sm rounded-0 rounded-top" data-bs-toggle="pill" data-bs-target="#rpi-instructions" type="button">
-                            {FontAwesome.RPi} Raspberry Pi
-                        </button>
-                    </li>
-                </ul>
-                <div className="tab-content bg-primary rounded-bottom rounded-end px-3 py-2" id="pills-tabContent">
-                    <div className="tab-pane fade show active" id="anrdoid-instructions">
-                        Add smartazanclock.com to your home screen and run it. That's it!
-                    </div>
-                    <div className="tab-pane fade" id="rpi-instructions">
-                        <a href="/setup/rpi/" className='whiteLink'>Instructions to run SmartAzanClock.com on a Raspberry Pi in kiosk mode.</a>
-                    </div>
-                </div>
+            <div className='fs-5'>
+                Add smartazanclock.com to your home screen and run it. That's it!
             </div>
-
-
             <p></p>
 
-            <div className='d-flex flex-row justify-content-between gap-1 align-items-center mt-2'>
+            <div className='d-flex flex-row justify-content-between gap-1 align-items-center mt-4'>
                 <div>
-                    <a className='btn btn-lg btn-dark' title='Privacy Policy' href="/privacy-policy/" target="_blank" rel="noreferrer">{FontAwesome.Shield}</a>
+                    <a className='whiteLink fs-2' title='Privacy Policy' href="/privacy-policy/" rel="noreferrer">{FontAwesome.Shield}</a>
                 </div>
                 <div>
-                    <a className='btn btn-lg btn-dark' title='Open Source Code' href="https://github.com/smartazanclock/pwa" target="_blank" rel="noreferrer">{FontAwesome.Github}</a>
+                    <a className='whiteLink fs-2' title='Open Source Code' href="https://github.com/smartazanclock/pwa" rel="noreferrer">{FontAwesome.Github}</a>
                 </div>
                 <div>
-                    <a className='btn btn-lg btn-dark' title='smartazanclock@gmail.com' href="mailto:smartazanclock@gmail.com">{FontAwesome.Envelope}</a>
+                    <a className='whiteLink fs-2' title='Prayer Times Chrome Extension' href="https://chrome.google.com/webstore/detail/prayer-times-chrome-exten/fbkmgnkliklgbmanjkmiihkdioepnkce">{FontAwesome.Chrome}</a>
                 </div>
                 <div>
-                    <a className='btn btn-lg btn-dark' title='Prayer Times Chrome Extension' href="https://chrome.google.com/webstore/detail/prayer-times-chrome-exten/fbkmgnkliklgbmanjkmiihkdioepnkce">{FontAwesome.Chrome}</a>
+                    <a className='whiteLink fs-2' href="/setup/rpi/" title='Raspberry Pi Adhan Clock Setup Instructions'>{FontAwesome.RPi}</a>
                 </div>
                 <div>
-                    <a className='btn btn-lg btn-dark' title='Readily set up devices on ebay' href="https://ebay.us/XF3NsK">{FontAwesome.Ebay}</a>
+                    <a className='whiteLink fs-2' title='smartazanclock@gmail.com' href="mailto:smartazanclock@gmail.com">{FontAwesome.Envelope}</a>
                 </div>
-
+                <div>
+                    <a className='whiteLink fs-2' title='Readily set up devices on ebay' href="https://ebay.us/XF3NsK">{FontAwesome.Ebay}</a>
+                </div>
             </div>
 
             <p></p>
